9695d7eeddc6c1c9c14ff4cc5efce67ebd12dab4
Alfonso Contreras <alfonso.contreras05@inacapmail.cl>
2023-07-14 11:40:09 -0400

    cards v1

--- js/lenceria.js
+++ js/lenceria.js
@@ -7,20 +7,45 @@ fetch("/js/catalogos/lenceria.json")
 
 function exibeProductos(productos) {
   productos.forEach(producto => {
-    catalogo.innerHTML += `
-      <div class='singleFilm card'>
-        <div class="front">
-          <img src="${producto.imagen}">
-        </div>
-        <div class="back">
-          <span class='infoProducto'>
-            ${producto.nombre}<hr>
-            ${producto.descripcion}<hr>
-            ${producto.precio}
-          </span>
-        </div>
-      </div>
-    `;
+    const card = document.createElement('div');
+    card.classList.add('flip-card');
+
+    const cardInner = document.createElement('div');
+    cardInner.classList.add('flip-card-inner');
+
+    const cardFront = document.createElement('div');
+    cardFront.classList.add('flip-card-front');
+
+    const cardBack = document.createElement('div');
+    cardBack.classList.add('flip-card-back');
+
+    const productImage = document.createElement('img');
+    productImage.classList.add('product-image');
+    productImage.src = producto.imagen;
+
+    const productName = document.createElement('p');
+    productName.classList.add('title');
+    productName.textContent = producto.nombre;
+
+    const productDescription = document.createElement('p');
+    productDescription.classList.add('description');
+    productDescription.textContent = producto.descripcion;
+
+    const productPrice = document.createElement('p');
+    productPrice.classList.add('price');
+    productPrice.textContent = producto.precio;
+
+    cardFront.appendChild(productImage);
+    cardBack.appendChild(productName);
+    cardBack.appendChild(productDescription);
+    cardBack.appendChild(productPrice);
+
+    cardInner.appendChild(cardFront);
+    cardInner.appendChild(cardBack);
+
+    card.appendChild(cardInner);
+
+    catalogo.appendChild(card);
   });
 }
 
