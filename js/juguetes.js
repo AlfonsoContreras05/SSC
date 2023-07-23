2df689a92c37f3f732dfa1cc7f33af9a9b476789
Alfonso Contreras <alfonso.contreras05@inacapmail.cl>
2023-07-23 12:20:21 -0400

    popup

--- js/juguetes.js
+++ js/juguetes.js
@@ -1,5 +1,3 @@
-// script.js
-
 fetch('/js/catalogos/juguetes.json')
   .then(response => response.json())
   .then(data => {
@@ -7,78 +5,45 @@ fetch('/js/catalogos/juguetes.json')
     const previewContainer = document.querySelector('.products-preview');
 
     data.productos.forEach(product => {
-      const productDiv = document.createElement('div');
-      productDiv.classList.add('product');
-      productDiv.setAttribute('data-name', product.nombre);
+      // Crear el contenido para mostrar en la vista previa
+      const previewContent = `
+        <div class="preview" data-target="${product.nombre}">
+          <img src="${product.imagen}" alt="${product.nombre}">
+          <h3>${product.nombre}</h3>
+          <p>${product.descripcion}</p>
+          <div class="category">Category: ${product.categoria}</div>
+          <div class="price">${product.precio}</div>
+          <i class="fas fa-times"></i>
+        </div>
+      `;
+      // Agregar el contenido al contenedor de vista previa
+      previewContainer.insertAdjacentHTML('beforeend', previewContent);
 
-      const productImage = document.createElement('img');
-      productImage.src = product.imagen;
-      productImage.alt = product.nombre;
+      // Crear el contenido del producto
+      const productContent = `
+        <div class="product" data-name="${product.nombre}">
+          <img src="${product.imagen}" alt="${product.nombre}">
+          <h3>${product.nombre}</h3>
+          <p>${product.descripcion}</p>
+          <div class="price">${product.precio}</div>
+        </div>
+      `;
+      // Agregar el contenido al contenedor de productos
+      productsContainer.insertAdjacentHTML('beforeend', productContent);
+    });
 
-      const productName = document.createElement('h3');
-      productName.textContent = product.nombre;
+    const previewBox = previewContainer.querySelectorAll('.preview');
 
-      const productRating = document.createElement('div');
-      productRating.classList.add('stars');
-      const rating = Math.floor(Math.random() * 5) + 1;
-      for (let i = 0; i < 5; i++) {
-        const starIcon = document.createElement('i');
-        if (i < rating) {
-          starIcon.classList.add('fas', 'fa-star');
-        } else if (i === rating) {
-          starIcon.classList.add('fas', 'fa-star-half-alt');
-        } else {
-          starIcon.classList.add('far', 'fa-star');
-        }
-        productRating.appendChild(starIcon);
-      }
-      const ratingCount = document.createElement('span');
-      ratingCount.textContent = `( ${Math.floor(Math.random() * 500)} )`;
-      productRating.appendChild(ratingCount);
-
-      const productDescription = document.createElement('p');
-      productDescription.textContent = product.descripcion;
-
-      const productPrice = document.createElement('div');
-      productPrice.classList.add('price');
-      productPrice.textContent = product.precio;
-
-      const buttonsDiv = document.createElement('div');
-      buttonsDiv.classList.add('buttons');
-
-      const buyButton = document.createElement('a');
-      buyButton.classList.add('buy');
-      buyButton.href = '#';
-      buyButton.textContent = 'buy now';
-
-      const cartButton = document.createElement('a');
-      cartButton.classList.add('cart');
-      cartButton.href = '#';
-      cartButton.textContent = 'add to cart';
-
-      buttonsDiv.appendChild(buyButton);
-      buttonsDiv.appendChild(cartButton);
-
-      productDiv.appendChild(productImage);
-      productDiv.appendChild(productName);
-      productDiv.appendChild(productRating);
-      productDiv.appendChild(productDescription);
-      productDiv.appendChild(productPrice);
-      productDiv.appendChild(buttonsDiv);
-
-      productsContainer.appendChild(productDiv);
-
-      productDiv.onclick = () => {
+    document.querySelectorAll('.products-container .product').forEach(product => {
+      product.onclick = () => {
         previewContainer.style.display = 'flex';
         previewBox.forEach(preview => {
-          if (preview.dataset.target === product.nombre) {
+          if (preview.dataset.target === product.getAttribute('data-name')) {
             preview.classList.add('active');
           }
         });
       };
     });
-
-    const previewBox = previewContainer.querySelectorAll('.preview');
 
     previewBox.forEach(close => {
       close.querySelector('.fa-times').onclick = () => {
