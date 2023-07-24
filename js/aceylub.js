90e6d73996c2acb707ea8f0e74ea93af318dcd67
Alfonso Contreras <alfonso.contreras05@inacapmail.cl>
2023-07-24 18:07:34 -0400

    filtrado

--- js/aceylub.js
+++ js/aceylub.js
@@ -1,65 +1,98 @@
+// Primero, obtén la referencia al campo de entrada de búsqueda
+const filterInput = document.getElementById('filter-input');
+
+// Asegúrate de que tus productos están disponibles en un scope más amplio
+let productsData;
+
 fetch('/js/catalogos/aceylub.json')
   .then(response => response.json())
   .then(data => {
-    const productsContainer = document.querySelector('.products-container');
-    const previewContainer = document.querySelector('.products-preview');
-
-    data.productos.forEach(product => {
-      // Crear el contenido para mostrar en la vista previa
-      const previewContent = `
-        <div class="preview" data-target="${product.nombre}">
-          <img src="${product.imagen}" alt="${product.nombre}">
-          <h3>${product.nombre}</h3>
-          <p>${product.descripcion}</p>
-          <div class="category">Categoría: ${product.categoria || 'Valor por definir'}</div>
-          <div class="price">${product.precio}</div>
-          <i class="fas fa-times"></i>
-        </div>
-      `;
-      // Agregar el contenido al contenedor de vista previa
-      previewContainer.insertAdjacentHTML('beforeend', previewContent);
-
-      
-      // Crear el contenido del producto
-      const productContent = `
-        <div class="product" data-name="${product.nombre}">
-          <img src="${product.imagen}" alt="${product.nombre}">
-          <h3>${product.nombre}</h3>
-          <p>${product.categoria || 'Valor por definir'}</p>
-          <div class="price">${product.precio}</div>
-        </div>
-      `;
-      // Agregar el contenido al contenedor de productos
-      productsContainer.insertAdjacentHTML('beforeend', productContent);
-    });
-
-    const previewBox = previewContainer.querySelectorAll('.preview');
-
-    document.querySelectorAll('.products-container .product').forEach(product => {
-      product.onclick = () => {
-        previewContainer.style.display = 'flex';
-        const productName = product.getAttribute('data-name');
-        previewBox.forEach(preview => {
-          if (preview.dataset.target === productName) {
-            preview.classList.add('active');
-          } else {
-            preview.classList.remove('active'); // Ocultar las vistas previas no seleccionadas
-          }
-        });
-      };
-    });
-
-    previewBox.forEach(close => {
-      close.querySelector('.fa-times').onclick = () => {
-        close.classList.remove('active');
-        previewContainer.style.display = 'none';
-      };
-    });
+    productsData = data.productos; // Almacena tus productos en la variable productsData
+    renderProducts(productsData);
   })
   .catch(error => {
     console.error('Error al cargar los productos:', error);
   });
 
+// La función de renderizado ahora se ocupa de un conjunto de productos que se pasan como argumento
+function renderProducts(products) {
+  const productsContainer = document.querySelector('.products-container');
+  const previewContainer = document.querySelector('.products-preview');
+
+  // Asegúrate de que los contenedores están vacíos antes de llenarlos
+  productsContainer.innerHTML = '';
+  previewContainer.innerHTML = '';
+
+  products.forEach(product => {
+    // Crear el contenido para mostrar en la vista previa
+    const previewContent = `
+        <div class="preview" data-target="${product.nombre}">
+          <img src="${product.imagen}" alt="${product.nombre}">
+          <h3>${product.nombre}</h3>
+          <p>${product.descripcion}</p>
+          <div class="category">Category: ${product.categoria}</div>
+          <div class="price">${product.precio}</div>
+          <i class="fas fa-times"></i>
+        </div>
+      `;
+
+    // Agregar el contenido al contenedor de vista previa
+    previewContainer.insertAdjacentHTML('beforeend', previewContent);
+
+    // Crear el contenido del producto
+    const productContent = `
+        <div class="product" data-name="${product.nombre}">
+          <img src="${product.imagen}" alt="${product.nombre}">
+          <h3>${product.nombre}</h3>
+          <p>${product.categoria}</p>
+          <div class="price">${product.precio}</div>
+        </div>
+      `;
+
+    // Agregar el contenido al contenedor de productos
+    productsContainer.insertAdjacentHTML('beforeend', productContent);
+  });
+
+  const previewBox = previewContainer.querySelectorAll('.preview');
+
+  document.querySelectorAll('.products-container .product').forEach(product => {
+    product.onclick = () => {
+      previewContainer.style.display = 'flex';
+      previewBox.forEach(preview => {
+        if (preview.dataset.target === product.getAttribute('data-name')) {
+          preview.classList.add('active');
+        }
+      });
+    };
+  });
+
+  previewBox.forEach(close => {
+    close.querySelector('.fa-times').onclick = () => {
+      close.classList.remove('active');
+      previewContainer.style.display = 'none';
+    };
+  });
+}
+
+// Agrega un event listener para la entrada del filtro
+filterInput.addEventListener('input', function() {
+  const filterValue = this.value.toLowerCase();
+
+  // Filtra los productos basado en el nombre
+  const filteredProducts = productsData.filter(product => product.nombre.toLowerCase().includes(filterValue));
+
+  // Luego, renderiza los productos filtrados
+  renderProducts(filteredProducts);
+
+  // Si no se encontraron productos, muestra un mensaje de error
+  if (filteredProducts.length === 0) {
+    document.getElementById('no-results-message').style.display = 'block';
+  } else {
+    document.getElementById('no-results-message').style.display = 'none';
+  }
+});
+
+// Event listener para el botón de regreso
 document.getElementById("back-button").addEventListener("click", function() {
   window.history.back(); // Regresar a la vista anterior
 });
--- js/lenceria.js
+++ js/lenceria.js
@@ -1,12 +1,31 @@
+// Primero, obtén la referencia al campo de entrada de búsqueda
+const filterInput = document.getElementById('filter-input');
+
+// Asegúrate de que tus productos están disponibles en un scope más amplio
+let productsData;
+
 fetch('/js/catalogos/lenceria.json')
   .then(response => response.json())
   .then(data => {
-    const productsContainer = document.querySelector('.products-container');
-    const previewContainer = document.querySelector('.products-preview');
+    productsData = data.productos; // Almacena tus productos en la variable productsData
+    renderProducts(productsData);
+  })
+  .catch(error => {
+    console.error('Error al cargar los productos:', error);
+  });
 
-    data.productos.forEach(product => {
-      // Crear el contenido para mostrar en la vista previa
-      const previewContent = `
+// La función de renderizado ahora se ocupa de un conjunto de productos que se pasan como argumento
+function renderProducts(products) {
+  const productsContainer = document.querySelector('.products-container');
+  const previewContainer = document.querySelector('.products-preview');
+
+  // Asegúrate de que los contenedores están vacíos antes de llenarlos
+  productsContainer.innerHTML = '';
+  previewContainer.innerHTML = '';
+
+  products.forEach(product => {
+    // Crear el contenido para mostrar en la vista previa
+    const previewContent = `
         <div class="preview" data-target="${product.nombre}">
           <img src="${product.imagen}" alt="${product.nombre}">
           <h3>${product.nombre}</h3>
@@ -16,12 +35,12 @@ fetch('/js/catalogos/lenceria.json')
           <i class="fas fa-times"></i>
         </div>
       `;
-      // Agregar el contenido al contenedor de vista previa
-      previewContainer.insertAdjacentHTML('beforeend', previewContent);
 
-      
-      // Crear el contenido del producto
-      const productContent = `
+    // Agregar el contenido al contenedor de vista previa
+    previewContainer.insertAdjacentHTML('beforeend', previewContent);
+
+    // Crear el contenido del producto
+    const productContent = `
         <div class="product" data-name="${product.nombre}">
           <img src="${product.imagen}" alt="${product.nombre}">
           <h3>${product.nombre}</h3>
@@ -29,113 +48,70 @@ fetch('/js/catalogos/lenceria.json')
           <div class="price">${product.precio}</div>
         </div>
       `;
-      // Agregar el contenido al contenedor de productos
-      productsContainer.insertAdjacentHTML('beforeend', productContent);
-    });
 
-    const previewBox = previewContainer.querySelectorAll('.preview');
-
-    document.querySelectorAll('.products-container .product').forEach(product => {
-      product.onclick = () => {
-        previewContainer.style.display = 'flex';
-        previewBox.forEach(preview => {
-          if (preview.dataset.target === product.getAttribute('data-name')) {
-            preview.classList.add('active');
-          }
-        });
-      };
-    });
-
-    previewBox.forEach(close => {
-      close.querySelector('.fa-times').onclick = () => {
-        close.classList.remove('active');
-        previewContainer.style.display = 'none';
-      };
-    });
-  })
-  .catch(error => {
-    console.error('Error al cargar los productos:', error);
+    // Agregar el contenido al contenedor de productos
+    productsContainer.insertAdjacentHTML('beforeend', productContent);
   });
 
-  document.getElementById("back-button").addEventListener("click", function() {
-    window.history.back(); // Regresar a la vista anterior
- });
+  const previewBox = previewContainer.querySelectorAll('.preview');
 
+  document.querySelectorAll('.products-container .product').forEach(product => {
+    product.onclick = () => {
+      previewContainer.style.display = 'flex';
+      previewBox.forEach(preview => {
+        if (preview.dataset.target === product.getAttribute('data-name')) {
+          preview.classList.add('active');
+        }
+      });
+    };
+  });
 
-//filtro
+  previewBox.forEach(close => {
+    close.querySelector('.fa-times').onclick = () => {
+      close.classList.remove('active');
+      previewContainer.style.display = 'none';
+    };
+  });
+}
 
-// Definimos la variable productsContainer y filterInput a nivel global
-const productsContainer = document.querySelector(".products-container");
-const noResultsMessage = document.getElementById("no-results-message");
-const filterInput = document.getElementById("filter-input");
+// Agrega un event listener para la entrada del filtro
+filterInput.addEventListener('input', function() {
+  const filterValue = this.value.toLowerCase();
 
-// Función para filtrar los productos según el término de búsqueda
-const filtrarProductos = (termino, productos) => {
-   // Convertimos el término de búsqueda a minúsculas para hacer la búsqueda case-insensitive
-   const terminoMin = termino.toLowerCase();
+  // Filtra los productos basado en el nombre
+  const filteredProducts = productsData.filter(product => product.nombre.toLowerCase().includes(filterValue));
 
-   // Filtramos los productos que coinciden con el término de búsqueda
-   const productosFiltrados = productos.filter((producto) => {
-      const nombreMin = producto.nombre.toLowerCase();
-      return nombreMin.includes(terminoMin);
-   });
+  // Luego, renderiza los productos filtrados
+  renderProducts(filteredProducts);
 
-   // Mostramos solo los productos filtrados o el mensaje de no coincidencias
-   if (productosFiltrados.length > 0) {
-      mostrarProductos(productosFiltrados);
-      noResultsMessage.style.display = "none";
-   } else {
-      noResultsMessage.style.display = "block";
-      productsContainer.innerHTML = "";
-   }
-};
-
-// Función para mostrar los productos en el contenedor
-const mostrarProductos = (productos) => {
-   // Limpiamos el contenedor de productos
-   productsContainer.innerHTML = "";
-
-   // Generamos dinámicamente el contenido de los productos y lo agregamos al contenedor
-   productos.forEach((producto) => {
-      // Creamos elementos para mostrar los detalles de cada producto
-      const productoDiv = document.createElement("div");
-      productoDiv.classList.add("producto");
-      productoDiv.innerHTML = `
-         <h4>${producto.nombre}</h4>
-         <p>Precio: ${producto.precio}</p>
-         <p>Categoría: ${producto.categoria}</p>
-         <img src="${producto.imagen}" alt="${producto.nombre}">
-      `;
-      productsContainer.appendChild(productoDiv);
-   });
-};
-
-// Función para cargar los datos desde el archivo JSON
-// Resto del código del archivo juguetes.js (mostrarProductos y cargarProductosDesdeJSON permanecen sin cambios)
-
-// Función para cargar los productos desde el archivo JSON
-const cargarProductosDesdeJSON = async () => {
-  try {
-    const response = await fetch('ruta-a-tu-archivo-json');
-    const data = await response.json();
-
-    const productos = data.productos;
-    mostrarProductos(productos);
-
-    // Agregar evento "input" para activar el filtrado en tiempo real
-    const filterInput = document.getElementById('filter-input');
-    filterInput.addEventListener('input', () => {
-      const terminoBusqueda = filterInput.value;
-      filtrarProductos(terminoBusqueda, productos);
-    });
-
-  } catch (error) {
-    console.log('Error al cargar los datos desde el archivo JSON:', error);
+  // Si no se encontraron productos, muestra un mensaje de error
+  if (filteredProducts.length === 0) {
+    document.getElementById('no-results-message').style.display = 'block';
+  } else {
+    document.getElementById('no-results-message').style.display = 'none';
   }
-};
+});
 
-// Resto del código del archivo juguetes.js (mostrarProductos permanece sin cambios)
+// Event listener para el botón de regreso
+document.getElementById("back-button").addEventListener("click", function() {
+  window.history.back(); // Regresar a la vista anterior
+});
 
+filterInput.addEventListener('input', function() {
+  const filterValue = this.value.toLowerCase();
 
-// Cargamos los productos desde el archivo JSON al cargar la página
-cargarProductosDesdeJSON();
+  // Filtra los productos basado en el nombre
+  const filteredProducts = productsData.filter(product => product.nombre.toLowerCase().includes(filterValue));
+
+  // Luego, renderiza los productos filtrados
+  renderProducts(filteredProducts);
+
+  const noResultsMessage = document.getElementById('no-results-message');
+
+  // Si no se encontraron productos, muestra un mensaje de error
+  if (filteredProducts.length === 0 && noResultsMessage) {
+    noResultsMessage.style.display = 'block';
+  } else if (noResultsMessage) {
+    noResultsMessage.style.display = 'none';
+  }
+});
